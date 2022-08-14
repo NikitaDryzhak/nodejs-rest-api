89703b80fcfb9daf8a6b954d654a285e163110f4
Nikita <dryzhak1@gmail.com>
2022-08-14 22:23:44 +0300

    second test

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,23 +3,23 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'HW2 test ' })
+  res.json({ message: 'HW2 test message' })
 })
 
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'test HW2 ' })
+  res.json({ message: 'test HW2 message' })
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'test HW2 ' })
+  res.json({ message: 'test HW2 message' })
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'test HW2 ' })
+  res.json({ message: 'test HW2 message' })
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'test HW2 ' })
+  res.json({ message: 'test HW2 message' })
 })
 
 module.exports = router
